edec4ae9ee0bff7460f10bdd7959258eb31b44ad
Jesus Alejandro Bañuelos Mendoza <76793906+Draxgun@users.noreply.github.com>
2023-11-17 15:21:19 -0600

    InitialCommit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import CardList from './Components/CardList/CardList';
+
+function App() {
+  return (
+    <div className="App">
+      <CardList/>
+      <a>asdasd</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Card.css";
+
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
+
+const Card = ({companyName,ticker,price}: Props) => {
+  return (
+  <div className='card'>
+    <img
+        src ='https://img.freepik.com/free-photo/front-view-funny-cute-dog-concept_23-2148786532.jpg?size=626&ext=jpg&ga=GA1.1.1937479475.1700193400&semt=sph'
+        alt = "Image"
+    />
+    <div className='details'>
+        <h2>{companyName} ({ticker})</h2>
+        <p>${price}</p>
+    </div>
+
+    <p className='info'>Lorem ipsum dolor, sit amet 
+    consectetur adipisicing elit. Libero, doloremque.
+    </p>
+
+    </div>
+  )
+}
+
+export default Card;
