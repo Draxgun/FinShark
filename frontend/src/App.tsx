6aef7b83c5f69e0075f668f02ab7107e9948ab83
Jesus Alejandro Bañuelos Mendoza <76793906+Draxgun@users.noreply.github.com>
2023-11-17 16:23:24 -0600

    Changes in events

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import CardList from './Components/CardList/CardList';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
+      <Search/>
       <CardList/>
-      <a>asdasd</a>
     </div>
   );
 }
